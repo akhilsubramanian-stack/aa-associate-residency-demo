2948fd5e986db7ac59fdb292c3ac0e7670e5bd13
akhilsubramanian-stack <akhil.subramanian@mosaicdigital.ae>
2026-08-16 23:22:51 +0400

    Update Dubai office Google Maps location

--- src/views/home/location.tsx
+++ src/views/home/location.tsx
@@ -6,7 +6,7 @@ export const Location = () => (
       <div className="office-location__map">
         <iframe
           title="A&A Associate Dubai office map"
-          src="https://www.google.com/maps?q=Rolex%20Tower%2C%20Sheikh%20Zayed%20Road%2C%20Dubai%2C%20UAE&output=embed"
+          src="https://www.google.com/maps?q=A%26A%20Associate%20LLC%20-%20Business%20Setup%20in%20Dubai%2C%20UAE&output=embed"
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
         />
@@ -16,7 +16,7 @@ export const Location = () => (
         <span>Dubai Headquarters</span>
         <h2 id="office-location-heading">Right at the heart of Dubai</h2>
         <p>Level 12, Rolex Tower, Sheikh Zayed Road, Near Financial Centre Metro Station, Dubai, UAE.</p>
-        <a href="https://maps.google.com/?q=Rolex+Tower+Sheikh+Zayed+Road+Dubai" target="_blank" rel="noreferrer">Open in Google Maps <b>↗</b></a>
+        <a href="https://google.com/maps/place/A%26A+Associate+LLC+-+Business+Setup+in+Dubai,+UAE/data=!4m2!3m1!1s0x0:0x714ad4b103d6ceaf?sa=X&ved=1t:2428&hl=en&ictx=111" target="_blank" rel="noreferrer">Open in Google Maps <b>↗</b></a>
       </div>
     </Inview>
   </section>
